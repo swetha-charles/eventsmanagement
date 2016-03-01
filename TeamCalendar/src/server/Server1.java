2eb641d66a0b480e4b709e1682e9d25498db4629
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-01 18:44:13 +0000

    Updated server0 -> server1,  client0 -> client1 and  added a protocol file in server. This program allows your to enter a number into client, the server will return double that value back to the client.

--- TeamCalendar/src/server/Server1.java
+++ TeamCalendar/src/server/Server1.java
@@ -6,16 +6,11 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class Server0 {
-	public static void main(String[] args) {
-		int portNumber;
-		if(args.length == 0){
-			portNumber = 7516;
-		}else{
-			portNumber = Integer.parseInt(args[0]);
-		}
-		
+public class Server1 {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		int portNumber = 7518;
 		
 		try(
 				ServerSocket serverListeningSocket = new ServerSocket(portNumber);
@@ -28,23 +23,20 @@ public class Server0 {
 			//System.out.println("Server: Setup successful");
 			String inputNumber, outputNumber;
 			
-			Protocol p = new Protocol();
+			Protocol np = new Protocol();
 			//System.out.println("Server: how is out?"+out.checkError());
 			//System.out.println("Server: how is in?" + in.ready());
 			while((inputNumber = in.readLine()) != null){
 				//System.out.println("Server: Within while loop and input was" + inputNumber);
-				outputNumber = p.processInput();
+				outputNumber = ""+np.processInput(Integer.parseInt(inputNumber));
 				//System.out.println("Server: Protocol finished, output was" + outputNumber);
 				out.println(outputNumber);
 				
 			}
 			
-		} catch (IOException e) {
-			portNumber++;
-			String[] newPort = {portNumber+""};
-			System.out.println("Server is now listening to port: " + portNumber);
-			Server0.main(newPort);
 		}
+		
+		
 		
 	}
 }
