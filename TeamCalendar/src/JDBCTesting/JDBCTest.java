8120138bdf3c74c8d74a21b28865e99f3fa4dc9b
mxw589 <mxw589@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-02 11:44:21 +0000

    Further testing for first attempts at establishing connection with the database - Currently unable to get JDBC driver to work as intended

--- TeamCalendar/src/JDBCTesting/JDBCTest.java
+++ TeamCalendar/src/JDBCTesting/JDBCTest.java
@@ -1,37 +1,56 @@
 package JDBCTesting;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class JDBCTest {
 
 	public static void main(String[] args) {
-	    int id;
-	    String subject;
-	    String permalink;
-	    
-	    
-	}
-	
-	public Connection connectToDatabase(){
-		Connection conn = null;
+		String protocol = "postgresql";
+		String driverName = "org.postgresql.Driver";
+		String server = "tinky-winky.cs.bham.ac.uk";
+		String port = "5432";
+		String db = "dbteach2";
+		String user = "mxw589";
+		String passwd = "ilovedatabases";
+
+		String URL ="jdbc:" + protocol + "://" + server + ":" + port + "/" + db;
+
 		
-	    try
-	    {
-	      Class.forName("org.postgresql.Driver");
-	      String url = "jdbc:postgresql://host:port/database";
-	      conn = DriverManager.getConnection(url,"THE_USER", "THE_PASSWORD");
-	    }
-	    catch (ClassNotFoundException e)
-	    {
-	      e.printStackTrace();
-	      System.exit(1);
-	    }
-	    catch (SQLException e)
-	    {
-	      e.printStackTrace();
-	      System.exit(2);
-	    }
-	    return conn;
+		System.out.println("-------- PostgreSQL JDBC Connection Testing ------------");
+
+		try {
+
+			Class.forName(driverName);
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? Include in your library path!");
+			e.printStackTrace();
+			return;
+
+		}
+
+		System.out.println("PostgreSQL JDBC Driver Registered!");
+
+		Connection connection = null;
+
+		try {
+
+			connection = DriverManager.getConnection(URL, user, passwd);
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
 	}
 }
--- TeamCalendar/src/JDBCTesting/Main.java
+++ TeamCalendar/src/JDBCTesting/Main.java
@@ -27,7 +27,7 @@ public class Main {
     public static void main(String[] args) {
 	// Load properties
 	try {
-	    FileInputStream stream = new FileInputStream(".properties");
+	    FileInputStream stream = new FileInputStream("test.properties");
 	    settings.load(stream);
 	}
 	catch (FileNotFoundException e) {
