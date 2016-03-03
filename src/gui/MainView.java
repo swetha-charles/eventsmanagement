458d7b97dfc675cee49e5a07556b2b3ef8e23db5
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-03 14:50:40 +0000

    Added to controller and a model

--- src/gui/MainView.java
+++ src/gui/MainView.java
@@ -1,20 +1,34 @@
 package gui;
 
-
-
-import java.awt.HeadlessException;
 import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
-public class MainView {
-	
-	public static void main(String[] args) throws HeadlessException, IOException {
+import controller.Controller;
+import model.Model;
+
+public class MainView extends JFrame implements Observer {
+
+	Model model = null;
+	Controller controller = null;
+
+	JLayeredPane login = null;
+	JPanel registration = null;
+
+	public MainView(Controller controller, Model model) throws IOException {
+		this.controller = controller;
+
+		this.login = new Login(this.controller);
+		this.registration = new RegistrationPanel(this.controller);
+		this.model = model;
 		
-		Login login = new Login();
 		JFrame frame = new JFrame("Calendar");
-//		frame.setLayout(new BorderLayout());
-		
+		// frame.setLayout(new BorderLayout());
+
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setContentPane(login);
@@ -23,4 +37,38 @@ public class MainView {
 		frame.setVisible(true);
 	}
 
+	public void addController(Controller controller) {
+		this.controller = controller;
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		switch (model.getCurrentState()) {
+		case REGISTRATION:
+			System.out.println("registration state");
+			this.getContentPane().removeAll();
+			this.setContentPane(registration);
+			this.setVisible(true);
+			
+
+		case LOGIN:
+			this.setContentPane(this.login);
+			break;
+
+		}
+
+	}
+
+	/*
+	 * public static void main(String[] args) throws HeadlessException,
+	 * IOException {
+	 * 
+	 * Login login = new Login(); JFrame frame = new JFrame("Calendar"); //
+	 * frame.setLayout(new BorderLayout());
+	 * 
+	 * JFrame.setDefaultLookAndFeelDecorated(true);
+	 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 * frame.setContentPane(login); frame.setSize(Integer.MAX_VALUE,
+	 * Integer.MAX_VALUE); frame.setResizable(true); frame.setVisible(true); }
+	 */
 }
