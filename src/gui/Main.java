24133df29ee1f38169cc18849b7a76d22d25c33e
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-03 21:16:56 +0000

    test

---
+++ src/gui/Main.java
@@ -0,0 +1,9 @@
+package gui;
+
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException, IOException {
+		TestView test = new TestView();
+	}
+}
