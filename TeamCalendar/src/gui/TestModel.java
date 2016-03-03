0cb0ed9d98d69566856a01aab3a6aa134cb36108
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-03 21:13:34 +0000

    testing

---
+++ TeamCalendar/src/gui/TestModel.java
@@ -0,0 +1,17 @@
+package gui;
+
+import java.util.Observable;
+
+public class TestModel extends Observable {
+	private static String state;
+	
+	public void changeState(){
+		setChanged();
+		System.out.println("testModel: changed state");
+		System.out.println("tstModel: observer count"+this.countObservers());
+		//System.out.println());
+		super.notifyObservers();
+		System.out.println("TestModel: Notified observers");
+	}
+	
+}
