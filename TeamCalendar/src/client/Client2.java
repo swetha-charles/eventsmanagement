1754df71a5c4c0452f9940cc4d0d6527b05d3db1
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-03 16:57:55 +0000

    Client2 is the correct one. There is weirdness going on right now.

--- TeamCalendar/src/client/Client2.java
+++ TeamCalendar/src/client/Client2.java
@@ -1,44 +1,37 @@
 package client;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 import controller.Controller;
 import gui.MainView;
 import model.Model;
 
 public class Client2 {
-	public static void main(String[] args) throws UnknownHostException, IOException{
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		System.out.println("Client started!");
-		
-		/*try (Socket clientSocket = new Socket("127.0.0.1", 5001);
-				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				Scanner fromUser = new Scanner(System.in);) {
 
-			
-		}*/
+		/*
+		 * try (Socket clientSocket = new Socket("127.0.0.1", 5001); PrintWriter
+		 * out = new PrintWriter(clientSocket.getOutputStream(), true);
+		 * BufferedReader in = new BufferedReader(new
+		 * InputStreamReader(clientSocket.getInputStream())); Scanner fromUser =
+		 * new Scanner(System.in);) {
+		 * 
+		 * 
+		 * }
+		 */
 
-		//start the GUI
+		// start the GUI
 		Controller controller = new Controller();
 		Model model = new Model(controller);
 		MainView view = new MainView(controller, model);
-		
-		//add an observer (view) to the model
+
+		// add an observer (view) to the model
 		model.addObserver(view);
 		controller.addModel(model);
 		controller.addView(view);
-		
-		while(true){
-			
-		}
-		
+
 	}
-	
-	
+
 }
