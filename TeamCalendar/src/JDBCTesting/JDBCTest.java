95b3cde2c996a0de3095b2a5d69a41b124c209f0
mxw589 <mxw589@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-03 21:39:23 +0000

    Now using the VPN rather than SSH, much easier! Successful queries for days!

--- TeamCalendar/src/JDBCTesting/JDBCTest.java
+++ TeamCalendar/src/JDBCTesting/JDBCTest.java
@@ -17,37 +17,12 @@ public class JDBCTest {
 
 		String protocol = "postgresql";
 		String driverName = "org.postgresql.Driver";
-		String server = "localhost";
-		int localport = 3366;
-		int uniport = 5432;
+		String server = "dbteach2.cs.bham.ac.uk";
 		String db = "team_chennai_calendar";
 		String user = "mxw589";
 		String dbpasswd = "ilovedatabases";
-
-		String URL ="jdbc:" + protocol + "://" + server + ":" + localport + "/" + db;
+		String URL ="jdbc:" + protocol + "://" + server + "/" + db;
 		Session session = null;
-
-		try {
-			JSch jsch=new JSch();
-
-			String host = "tinky-winky.cs.bham.ac.uk";
-
-			session=jsch.getSession(user, host, 22);
-
-			String sshpasswd = "Mo0money!";
-			session.setPassword(sshpasswd);
-
-			session.setConfig("StrictHostKeyChecking", "no");
-
-			System.out.println("Connecting to SSH...");
-			session.connect(5000);
-
-			session.setPortForwardingL(localport, "dbteach2", uniport);
-			System.out.println("Connected to SSH");
-		} catch (JSchException e1) {
-			e1.printStackTrace();
-			System.exit(-1);
-		}
 
 		System.out.println("-------- PostgreSQL JDBC Connection Testing ------------");
 
@@ -102,17 +77,23 @@ public class JDBCTest {
 			ResultSet rs = stmt.executeQuery(query);
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int columnsNumber = rsmd.getColumnCount();
-
+			int columnWidth = 20;
+			
+			
 			for (int i = 1; i <= columnsNumber; i++) {
 				if (i > 1) System.out.print("|  ");
-				System.out.print(rsmd.getColumnName(i));
+				String columnName = rsmd.getColumnName(i);
+				
+				while(columnName.length() <20){ columnName = columnName + " ";}
+				System.out.print(columnName);
 			}
 			System.out.println("");
-
+			
 			while (rs.next()) {
 				for (int i = 1; i <= columnsNumber; i++) {
 					if (i > 1) System.out.print("|  ");
 					String columnValue = rs.getString(i);
+					while(columnValue.length() <20){ columnValue = columnValue + " ";}
 					System.out.print(columnValue);
 				}
 				System.out.println("");
@@ -133,28 +114,6 @@ public class JDBCTest {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
-		try{
-			String command="exit";
-			Channel channel=session.openChannel("exec");
-			((ChannelExec)channel).setCommand(command);
-
-			OutputStream out=channel.getOutputStream();
-
-			channel.connect();
-			out.write(command.getBytes()); out.flush();
-			
-			try{Thread.sleep(1000);}catch(Exception e){System.out.println(e);}
-
-			channel.disconnect();
-			session.disconnect();
-
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
-
-
 
 	}
 
