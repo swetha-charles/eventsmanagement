8c483b37f14f12a66c5dad4fe0e56b25263257cd
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-03 21:09:29 +0000

    Nothing major, added a test controller. This will need to be taken out later. It's useful for testing purposes right now.

--- TeamCalendar/src/gui/Login.java
+++ TeamCalendar/src/gui/Login.java
@@ -26,6 +26,7 @@ public class Login extends JPanel{
 	private static final long serialVersionUID = 1L;
 	JLabel background;
 	Controller controller;
+	TestController Tcontroller;
 
 	/**Constructor to create a Login
 	 * 
@@ -71,9 +72,50 @@ public class Login extends JPanel{
 		background.setOpaque(true);
 	}
 	
+	public Login(TestController controller) throws IOException{
+		this.Tcontroller = controller;
+		
+		GridBagLayout layout = new GridBagLayout();
+		setLayout(layout);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridwidth = 3;
+		gbc.gridheight = 3;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+//		gbc.anchor = GridBagConstraints.BASELINE;
+//		gbc.fill = GridBagConstraints.BOTH;
+		
+		//method that reads picture from file to set as background
+		background = new JLabel() {
+		
+			private static final long serialVersionUID = 1L;
+			private Image backgroundImage = ImageIO.read(new File("calendar.jpg"));
+			public void paint( Graphics g ) { 
+			    super.paint(g);
+			    g.drawImage(backgroundImage, 0, 0, null);
+			  }
+			};
+		
+		add(background, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		gbc.weightx = 0;
+		gbc.weighty = 0;
+		LoginPanel loginPanel = new LoginPanel(this.Tcontroller);
+		add(loginPanel, gbc);
+		
+		loginPanel.setOpaque(true);
+		background.setOpaque(true);
+	}
+	
+	
 	public static void main(String[] args) throws IOException {
 		
-		JFrame frame = new JFrame();
+	/*	JFrame frame = new JFrame();
 		Controller controller = new Controller();
 		
 		Login loginPanel = new Login(controller);
@@ -83,7 +125,7 @@ public class Login extends JPanel{
 		frame.setContentPane(loginPanel);
 		frame.setSize(Integer.MAX_VALUE, Integer.MAX_VALUE);
 		frame.setResizable(true);
-		frame.setVisible(true);
+		frame.setVisible(true)*/;
 	}
 }
 
