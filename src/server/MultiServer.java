8a302a54bdc5da885b3153373e360f0d507f20ac
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-03 14:52:05 +0000

    Multiserver can accept connections and start a thread to to deal with this socket

--- src/server/MultiServer.java
+++ src/server/MultiServer.java
@@ -1,28 +1,40 @@
 package server;
+
 import java.io.*;
 import java.net.*;
 
 public class MultiServer {
 	public static void main(String[] args) {
-		int portNumber1 = 5000;
-		/*int portNumber2 = 10000;
-		int portNumber3 = 20000;*/
-		
+		int portNumber1 = 5001;
+		ServerSocket serverSocket1 = null;
+		/*
+		 * int portNumber2 = 10000; int portNumber3 = 20000;
+		 */
+
 		boolean listening = true;
 
-		try (ServerSocket serverSocket1 = new ServerSocket(portNumber1)) {
+		try {
+			serverSocket1 = new ServerSocket(portNumber1);
 			while (listening) {
-				
-				//.accept() creates a new socket!
-				
+				System.out.println("Server now listening on port:" + portNumber1);
+				// .accept() creates a new socket!
+
 				new MultiServerThread(serverSocket1.accept()).start();
 			}
-		} catch (IOException e) {
 			
+		} catch (IOException e) {
+
 			System.out.println("Could not listen on port " + portNumber1);
 			System.exit(-1);
-			
+
+		} finally {
+			try {
+				serverSocket1.close();
+			} catch (IOException e) {
+				System.out.println("Could not close socket listening on " + portNumber1);
+				e.printStackTrace();
+			}
 		}
-		
+
 	}
 }
