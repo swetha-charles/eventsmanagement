d64284fe38382d450a215f26230e38634f18b1d1
mxw589 <mxw589@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-04 11:35:42 +0000

    Initial exploratory work done into transferring objects over sockets

--- TeamCalendar/src/server/ThreadForClient.java
+++ TeamCalendar/src/server/ThreadForClient.java
@@ -1,17 +1,31 @@
 package server;
 
+import java.io.*;
 import java.net.*;
 
 public class ThreadForClient implements Runnable{
 
 	Socket clientSocket;
+	ObjectInputStream fromClient;
+	ObjectOutputStream toClient;
+	boolean loggedIn;
 	
 	public ThreadForClient(Socket clientSocket){
 		this.clientSocket = clientSocket;
+		try {
+			fromClient = new ObjectInputStream(clientSocket.getInputStream());
+			toClient = new ObjectOutputStream(clientSocket.getOutputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
-	public void run() {		
+	public void run() {
+		while(loggedIn = true){
+			
+		}
 	}
 
 }	
