b46df3c2f969fdfc12a4a0628b2d75b1ea1f6191
mxw589 <mxw589@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-04 11:35:42 +0000

    Initial exploratory work done into transferring objects over sockets

--- src/server/ObjectServer.java
+++ src/server/ObjectServer.java
@@ -7,13 +7,13 @@ import java.io.*;
 public class ObjectServer {
 
 	public static void main(String[] args) throws Exception {
+		
 		ServerSocket ss = new ServerSocket(4444);
 
 		ArrayList<Thread> clientThreads = new ArrayList<Thread>();
 		
 		while(true){
 			clientThreads.add(new Thread(new ThreadForClient(ss.accept())));
-			
 		}
 	}
 }
