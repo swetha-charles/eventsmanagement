852f87aa0571e2821b608b3cf6d2d6ecf5e97c97
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-04 21:32:09 +0000

    Merged changes with Mark's edits

--- src/server/ObjectClient.java
+++ src/server/ObjectClient.java
@@ -1,6 +1,11 @@
 package server;
 
 import java.net.*;
+
+import controller.Controller;
+import gui.MainView;
+import model.Model;
+
 import java.io.*;
 
 public class ObjectClient {
@@ -10,6 +15,23 @@ public class ObjectClient {
 		Socket s = new Socket("localhost", 4444);
 		ObjectOutputStream toServer = new ObjectOutputStream(s.getOutputStream());
 		ObjectInputStream fromServer = new ObjectInputStream(s.getInputStream());
+	    
+	    Controller controller = new Controller(toServer, fromServer);
+		Model model = new Model(controller);
+		MainView view = new MainView(controller, model);
+
+		// add an observer (view) to the model
+		model.addObserver(view);
+		controller.addModel(model);
+		controller.addView(view);
+		
+		try {
+			OTUsernameCheck p = (OTUsernameCheck) fromServer.readObject();
+			System.out.println(p.getAlreadyExists());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		toServer.writeObject(new OTUsernameCheck("mwizzle"));
 
