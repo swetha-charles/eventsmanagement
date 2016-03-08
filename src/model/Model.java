48c737f9c8656b3d7b4723d7be2742e2889a6cc2
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-08 21:22:19 +0000

    username is hooked up to respond to duplicates

--- src/model/Model.java
+++ src/model/Model.java
@@ -7,6 +7,8 @@ import server.ObjectClientController;
 public class Model extends Observable{
 	private State currentstate;
 	private ObjectClientController controller;
+	private boolean RegUsernameexists = false;
+	private boolean RegEmailexists = false;
 	
 	public Model(ObjectClientController controller2){
 		this.controller = controller2;
@@ -21,14 +23,39 @@ public class Model extends Observable{
 		
 	}
 	
+	
+	public boolean getRegUsernameExists() {
+		return RegUsernameexists;
+	}
+
+
+
+	public boolean getRegEmailExists() {
+		return RegEmailexists;
+	}
+
+
+
+	public void setRegUsernameExists(boolean bool){
+		this.RegUsernameexists = bool;
+		this.changeCurrentState(State.REGISTRATIONUPDATE);
+		
+	}
+	
+	public void setRegEmailExists(boolean bool){
+		this.RegUsernameexists = bool;
+		this.changeCurrentState(State.REGISTRATIONUPDATE);
+	
+	}
+	
 	public synchronized void changeCurrentState(State state){
 		System.out.println("Model: has how many observers?"+this.countObservers());
 		this.currentstate = state;
-		System.out.println("Model: Model's state changed to " + this.currentstate);
+		//System.out.println("Model: Model's state changed to " + this.currentstate);
 		this.setChanged();
-		System.out.println("Model: About to notify observers!");
+		//System.out.println("Model: About to notify observers!");
 		super.notifyObservers();
-		System.out.println("Model: Notified observers!");
+		//System.out.println("Model: Notified observers!");
 		
 	}
 
