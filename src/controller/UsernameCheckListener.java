bf71b5b7b251eba06c9bf7d1d72e9cfd7323265a
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-08 15:54:10 +0000

    Idk if i need this but it's getting committed anyway

--- src/controller/UsernameCheckListener.java
+++ src/controller/UsernameCheckListener.java
@@ -5,7 +5,6 @@ import java.awt.event.FocusListener;
 
 public interface UsernameCheckListener extends FocusListener{
 
-	public void usernameFocusGained(FocusEvent e);
-	public void usernameFocusLost(FocusEvent e);
+	
 
 }
