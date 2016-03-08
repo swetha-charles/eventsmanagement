6c9deaa330bcfbb528f6cf69ccdc5281f104034b
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-08 15:55:28 +0000

    Sending OTUSernameCheck between client and server. Made some changes to implement this.

---
+++ TeamCalendar/src/server/OTEmailCheck.java
@@ -0,0 +1,27 @@
+package server;
+
+public class OTEmailCheck extends ObjectTransferrable{
+	private String email;
+	private boolean alreadyExists;
+	
+	public OTEmailCheck(String email){
+		setOpCode("0002");
+		this.email= email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean getAlreadyExists() {
+		return alreadyExists;
+	}
+
+	public void setAlreadyExists(boolean alreadyExists) {
+		this.alreadyExists = alreadyExists;
+	}
+}
