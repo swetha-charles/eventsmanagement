c031aacb81a0e05b53b2981c7fc15e717258b4c1
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-08 15:54:41 +0000

    Getting to grips with focus listeners looking into username duplication and email duplication.

--- TeamCalendar/src/gui/Main.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-import java.io.IOException;
-
-public class Main {
-	public static void main(String[] args) throws InterruptedException, IOException {
-		TestView test = new TestView();
-	}
-}
