af208ffcb7301e9fcf56a1ce04f09de9b0946a0a
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-09 13:17:36 +0000

    Renamed OTSFromServer

---
+++ src/server/ThreadForServer.java
@@ -0,0 +1,66 @@
+package server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import model.Model;
+
+public class ThreadForServer implements Runnable{
+	LinkedBlockingQueue<ObjectTransferrable> OT;
+	private ObjectClientController occ;
+	private ObjectInputStream fromServer;
+	private ObjectOutputStream toServer;
+	private Model model;
+	
+	public ThreadForServer(ObjectClientController occ, ObjectInputStream fromServer, ObjectOutputStream toServer, Model model){
+		this.OT = new LinkedBlockingQueue<ObjectTransferrable>();
+		this.fromServer = fromServer;
+		this.toServer = toServer;
+		this.model = model;
+		
+	}
+
+	@Override
+	public void run() {
+		while(true){
+			ObjectTransferrable receivedOperation = null;
+			try {
+				receivedOperation = (ObjectTransferrable) this.fromServer.readObject();
+				this.runOT(receivedOperation);
+			} catch (ClassNotFoundException e) {
+				// When the ObjectTransferrable isn't the right class
+				e.printStackTrace();
+			} catch (IOException e) {
+				// When the fromServer is not working ;
+			} 
+		}
+		
+	}
+	
+	private void runOT(ObjectTransferrable receivedOperation){
+		switch(receivedOperation.getOpCode()){
+		case "0001":
+			OTUsernameCheck otuc = (OTUsernameCheck) receivedOperation;
+			if(otuc.getAlreadyExists()){
+				this.model.setUsername(otuc.getUsername());
+				this.model.setUsernameExists(true);
+			} else if(!otuc.getAlreadyExists()){
+				this.model.setUsername(otuc.getUsername());
+				this.model.setUsernameExists(false);
+			}
+			
+		case "0002":
+			OTEmailCheck otec = (OTEmailCheck) receivedOperation;
+			if(otec.getAlreadyExists()){
+				this.model.setEmail(otec.getEmail());
+				this.model.setEmailExists(true);
+			} else if(!otec.getAlreadyExists()){
+				this.model.setEmail(otec.getEmail());
+				this.model.setEmailExists(false);
+			}
+		}
+	}
+	
+}
