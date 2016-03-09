bd78af70906bf61921751208002577a881eb4ba5
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-09 12:44:19 +0000

    View no longer directly communicates with the controller. It goes now via model, to the controller.

--- TeamCalendar/src/model/Model.java
+++ TeamCalendar/src/model/Model.java
@@ -1,63 +1,90 @@
 package model;
 
 import java.util.Observable;
-
 import server.ObjectClientController;
 
-public class Model extends Observable{
+public class Model extends Observable {
 	private State currentstate;
 	private ObjectClientController controller;
+
+	private String username;
+	private String email;
+	
+
 	private boolean RegUsernameexists = false;
 	private boolean RegEmailexists = false;
-	
-	public Model(ObjectClientController controller2){
+
+	public Model(ObjectClientController controller2) {
 		this.controller = controller2;
-		
+
 		this.currentstate = State.LOGIN;
 	}
-	
-	
-	
-	public State getCurrentState(){
-		return this.currentstate;
-		
-	}
-	
-	
-	public boolean getRegUsernameExists() {
-		return RegUsernameexists;
+
+	public void checkUsername(String username) {
+		this.username = username;
+		this.controller.checkUsername(username);
 	}
 
-
+	public void checkEmail(String email) {
+		this.email = email;
+		this.controller.checkEmail(email);
+	}
 
 	public boolean getRegEmailExists() {
 		return RegEmailexists;
 	}
 
+	public boolean getRegUsernameExists() {
+		return RegUsernameexists;
+	}
 
-
-	public void setRegUsernameExists(boolean bool){
+	public void setRegUsernameExists(boolean bool) {
 		this.RegUsernameexists = bool;
 		this.changeCurrentState(State.REGISTRATIONUPDATE);
-		
+
 	}
-	
-	public void setRegEmailExists(boolean bool){
+
+	public void setRegEmailExists(boolean bool) {
 		this.RegUsernameexists = bool;
 		this.changeCurrentState(State.REGISTRATIONUPDATE);
+
+	}
+
 	
+
+	public State getCurrentState() {
+		return this.currentstate;
+
 	}
 	
-	public synchronized void changeCurrentState(State state){
-		System.out.println("Model: has how many observers?"+this.countObservers());
+	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public synchronized void changeCurrentState(State state) {
+		System.out.println("Model: has how many observers?" + this.countObservers());
 		this.currentstate = state;
-		//System.out.println("Model: Model's state changed to " + this.currentstate);
+		// System.out.println("Model: Model's state changed to " +
+		// this.currentstate);
 		this.setChanged();
-		//System.out.println("Model: About to notify observers!");
+		// System.out.println("Model: About to notify observers!");
 		super.notifyObservers();
-		//System.out.println("Model: Notified observers!");
-		
+		// System.out.println("Model: Notified observers!");
+
 	}
 
-	
 }
