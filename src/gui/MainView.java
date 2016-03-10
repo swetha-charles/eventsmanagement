a6297bee3741e753002f6378528fc3ace5edc43e
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-10 23:54:06 +0000

    Model has more business logic

--- src/gui/MainView.java
+++ src/gui/MainView.java
@@ -9,11 +9,13 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+import controller.*;
 import model.Model;
-import model.State;
+import model.ModelState;
 import server.ObjectClientController;
 
 public class MainView extends JFrame implements Observer {
@@ -32,64 +34,20 @@ public class MainView extends JFrame implements Observer {
 		this.login = new Login(this.controller);
 
 		this.model = model;
-		
+
 		scroll = new JScrollPane(login);
-	
 
 		setDefaultLookAndFeelDecorated(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setContentPane(scroll);
 		setSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
-
 		setResizable(true);
 		setVisible(true);
-		addWindowListener(new WindowListener() {
-
-			@Override
-			public void windowOpened(WindowEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void windowClosing(WindowEvent e) {
-				MainView.this.model.changeCurrentState(State.EXIT);
-
-			}
-
-			@Override
-			public void windowClosed(WindowEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void windowIconified(WindowEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void windowDeiconified(WindowEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void windowActivated(WindowEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void windowDeactivated(WindowEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-		});
+		this.addWindowListener((WindowClosingListener) (e) -> model.changeCurrentState(ModelState.EXIT));
+		
+		
 	}
-	
+
 	public JPanel getLogin() {
 		return login;
 	}
@@ -101,7 +59,7 @@ public class MainView extends JFrame implements Observer {
 	public void setLogin(JPanel login) {
 		this.login = login;
 	}
-	
+
 	public void setScroll(JScrollPane scroll) {
 		this.scroll = scroll;
 	}
@@ -113,56 +71,53 @@ public class MainView extends JFrame implements Observer {
 	// Observer
 	@Override
 	public synchronized void update(Observable o, Object arg) {
-
-		switch (model.getCurrentState()) {
-
-		case REGISTRATION:
-			this.registration = new Registration(controller, model);
-			JScrollPane scroll = new JScrollPane(this.registration);
-			this.setContentPane(scroll);
-			this.validate();
-			break;
-		case LOGIN:
+		
+		if (model.getCurrentState().equals(ModelState.EXIT)) {
 			
-			JScrollPane scroll1 = new JScrollPane(login);
-			this.setContentPane(scroll1);
+			this.dispose();
+
+		} else if (model.getCurrentState().equals(ModelState.ERRORCONNECTIONDOWN)) {
+
+			JOptionPane.showMessageDialog(this, "Connection down, check network");
+		
+		} else {
+		
+			this.setContentPane(model.getCurrentPanel());
+			
 			this.validate();
-			break;
-
-		case REGISTRATIONUPDATE:
-			if(!model.getEmailMatchesRegex()){
-				this.registration.getRegistrationPanel().setEmailLabel("Email incorrect format*");
-				repaint();
-				break;
-			} else {
-				this.registration.getRegistrationPanel().setEmailLabel("Email*");
-			}
-			if (model.getUsernameExists()) {
-				this.registration.getRegistrationPanel().setUserLabel("Username already exists!*");
-			} else {
-				this.registration.getRegistrationPanel().setUserLabel("Username*");
-			}
-
-			if (model.getEmailExists()) {
-				this.registration.getRegistrationPanel().setEmailLabel("Email already exists!*");
-			} else {
-				this.registration.getRegistrationPanel().setEmailLabel("Email*");
-			}
-			this.repaint();
-			break;
-
-
-		case EXIT:
-			dispose();
-		default:
-			break;
-
 		}
 
+		/*
+		 * switch (model.getCurrentState()) {
+		 * 
+		 * case REGISTRATION: this.registration = new Registration(controller,
+		 * model); JScrollPane scroll = new JScrollPane(this.registration);
+		 * this.setContentPane(scroll); this.validate(); break; case LOGIN:
+		 * 
+		 * JScrollPane scroll1 = new JScrollPane(login);
+		 * this.setContentPane(scroll1); this.validate(); break;
+		 * 
+		 * case REGISTRATIONUPDATE: if(!model.getEmailMatchesRegex()){
+		 * this.registration.getRegistrationPanel().setEmailLabel(
+		 * "Email incorrect format*"); repaint(); break; } else {
+		 * this.registration.getRegistrationPanel().setEmailLabel("Email*"); }
+		 * if (model.getUsernameExists()) {
+		 * this.registration.getRegistrationPanel().setUserLabel(
+		 * "Username already exists!*"); } else {
+		 * this.registration.getRegistrationPanel().setUserLabel("Username*"); }
+		 * 
+		 * if (model.getEmailExists()) {
+		 * this.registration.getRegistrationPanel().setEmailLabel(
+		 * "Email already exists!*"); } else {
+		 * this.registration.getRegistrationPanel().setEmailLabel("Email*"); }
+		 * this.repaint(); break;
+		 * 
+		 * 
+		 * case EXIT: dispose(); default: break;
+		 * 
+		 * }
+		 */
 	}
-	
-	
-
 
 	/*
 	 * public static void main(String[] args) throws HeadlessException,
