7c5e2ed8981516056b18338cddfaefe6e20b1a76
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-10 23:55:14 +0000

    Model has more business logic

---
+++ src/objectTransferrable/OTExitGracefully.java
@@ -0,0 +1,10 @@
+package objectTransferrable;
+
+public class OTExitGracefully extends ObjectTransferrable {
+	private static final long serialVersionUID = 1L;
+	
+	public OTExitGracefully(){
+		setOpCode("0005");
+		
+	}
+}
