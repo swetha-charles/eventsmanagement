0cf642f5f9b995c21283d98a4b353706f2e3ee11
tmd668 <tmd668@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-11 22:12:36 +0000

    Filled out OTRegistrationInformationConfirmation and updated package info with this methods opcode

--- TeamCalendar/src/objectTransferrable/OTRegistrationInformationConfirmation.java
+++ TeamCalendar/src/objectTransferrable/OTRegistrationInformationConfirmation.java
@@ -9,5 +9,31 @@ package objectTransferrable;
  *
  */
 public class OTRegistrationInformationConfirmation extends ObjectTransferrable {
+	private boolean registrationSuccess;
+	private String whatFailed, reasonForFailure, opcode;
+	
+	
+	public OTRegistrationInformationConfirmation(boolean registrationSuccess, String whatFailed, String reasonForFailure) {
+		opcode = "0006";
+		this.registrationSuccess = registrationSuccess;
+		this.whatFailed = whatFailed;
+		this.reasonForFailure = reasonForFailure;
+	}
+	
+	public boolean isRegistrationSuccess() {
+		return registrationSuccess;
+	}
+
+	public String getWhatFailed() {
+		return whatFailed;
+	}
+
+	public String getReasonForFailure() {
+		return reasonForFailure;
+	}
+
+	public String getOpcode() {
+		return opcode;
+	}
 
 }
