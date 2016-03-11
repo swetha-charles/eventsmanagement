6ad469763ff223c1bd49ab8ac47c82234725cbfa
tmd668 <tmd668@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-11 22:03:00 +0000

    Object for registration confirmation

---
+++ src/objectTransferrable/OTRegistrationInformationConfirmation.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package objectTransferrable;
+
+/**
+ * Confirmation Object for a attempted registration
+ * @author tmd668
+ *
+ */
+public class OTRegistrationInformationConfirmation extends ObjectTransferrable {
+
+}
