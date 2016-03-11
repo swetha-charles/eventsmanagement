84fc3eb7f563a16bdb21c92fbf09a32b9233de34
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-11 03:37:40 +0000

    Client package created with requisite classes.  made listener package too

---
+++ TeamCalendar/src/listener/interfaces/FocusLostListener.java
@@ -0,0 +1,14 @@
+package listener.interfaces;
+
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public interface FocusLostListener extends FocusListener{
+	
+	@Override
+	public default void focusGained(FocusEvent e){
+		
+	}
+	
+	
+}
