99f5591a7c48dbed2ed2b794b30e99e4b7fb9463
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-13 18:22:45 +0000

    Cleaned up unnecessary code

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,18 +4,12 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Calendar;
 
 import gui.MainView;
 import model.Model;
 import model.ModelState;
-import objectTransferrable.OTEmailCheck;
-import objectTransferrable.OTExitGracefully;
-import objectTransferrable.OTLogin;
-import objectTransferrable.OTLoginSuccessful;
-import objectTransferrable.OTRegistrationInformation;
-import objectTransferrable.OTUsernameCheck;
-import objectTransferrable.ObjectTransferrable;
+import objectTransferrable.*;
 
 public class Client {
 	private int portnumber;
@@ -66,7 +60,9 @@ public class Client {
 
 		}
 	}
-
+	
+	
+	//read from server
 	public void readFromServer(String waitingForOpcode) {
 		ObjectTransferrable receivedOperation = null;
 		try {
@@ -75,7 +71,13 @@ public class Client {
 			if (!receivedOperation.getOpCode().equals(waitingForOpcode)) {
 				System.out.println("Client was expecting OT with OpCode " + waitingForOpcode
 						+ "but received OT with OpCode " + receivedOperation.getOpCode());
-				// should we cause an error here? throw new RuntimeException();
+				//throw new UnexpectedOTReceivedException();
+				
+				//should
+				//we
+				//throw
+				//an exception
+				//here??
 			}
 			this.runOT(receivedOperation);
 		} catch (ClassNotFoundException e) {
@@ -112,6 +114,10 @@ public class Client {
 				this.model.setEmailExists(false);
 			}
 			break;
+		case "0009":
+			OTReturnDayEvents eventsObject = (OTReturnDayEvents) receivedOperation;
+			this.model.displayEvents(eventsObject.getEventList());
+			break;
 		case "0013":
 			OTLoginSuccessful loginObject = (OTLoginSuccessful) receivedOperation;
 			if (loginObject.isLoginSuccessful()) {
@@ -130,12 +136,13 @@ public class Client {
 			break;
 		}
 	}
-
+	
+	//---------------- writeToServer calls -----------------------//
 	public void checkUsername(String username) {
 		OTUsernameCheck otuc = new OTUsernameCheck(username);
 
 		this.writeToServer(otuc, false, otuc.getOpCode());
-		System.out.println("Sent OT with opcode " + otuc.getOpCode());
+		System.out.println("Sent OT with opcode" + otuc.getOpCode());
 
 	}
 
@@ -143,14 +150,14 @@ public class Client {
 		OTEmailCheck otec = new OTEmailCheck(email);
 
 		this.writeToServer(otec, false, otec.getOpCode());
-		System.out.println("Client: Sent OT with opcode " + otec.getOpCode());
+		System.out.println("Client: Sent OT with opcode" + otec.getOpCode());
 
 	}
 
 	public void checkRegistration(OTRegistrationInformation otri) {
-		String complementOpCode = "0006";
-		System.out.println("Client: Send OT with opcode " + otri.getOpCode());
+		String complementOpCode = "0004";
 		this.writeToServer(otri, false, complementOpCode);
+		System.out.println("Client: Send OT with opcode " + otri.getOpCode());
 		System.out.println("Client: Expecting OT with opcode " + complementOpCode);
 	}
 
@@ -160,7 +167,20 @@ public class Client {
 		System.out.println("Client: Send OT with opcode " + loginObject.getOpCode());
 		System.out.println("Client: Expecting OT with opcode " + complementOpCode);
 	}
-
+	
+	public void getMeetingsForToday(String username){
+		String complementOpCode = "0009";
+		Calendar now = Calendar.getInstance();
+		OTRequestMeetingsOnDay getMeetingsObject = new OTRequestMeetingsOnDay(username, now);
+		this.writeToServer(getMeetingsObject, false, complementOpCode);
+		System.out.println("Client: Send OT with opcode " + getMeetingsObject.getOpCode());
+		System.out.println("Client: Expecting OT with opcode " + complementOpCode);
+	}
+	//--------------------- writeToServer calls Ends---------------------------//
+	
+	
+	
+	//--------------------- Exit -------------------------------------//
 	public void exitGracefully() {
 		if (s != null) {
 			OTExitGracefully oeg = new OTExitGracefully();
@@ -186,7 +206,6 @@ public class Client {
 	}
 
 	private void attemptToCloseConnections() {
-		int safeClosure = 0;
 		if (s != null) {
 			try {
 				s.shutdownOutput();
@@ -223,77 +242,6 @@ public class Client {
 		}
 		model.promptRestart();
 
-	}
-
-	@SuppressWarnings("unused")
-	private void deprecatedAttemptRecovery() {
-		this.attemptToCloseConnections();
-		long startTime = System.currentTimeMillis();
-		long endTime = System.currentTimeMillis();
-
-		while (!this.model.getCurrentState().equals(ModelState.EXIT)) {
-			// allows the user ~16.67 minutes to get their shit together
-			if (endTime - startTime < 1000000) {
-				try {
-					this.s = new Socket("localhost", portnumber);
-					break;
-				} catch (IOException e) {
-					System.out.println(
-							"Connection has not yet been restored. Client will sleep for 1 second, then try again");
-					Thread.currentThread();
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e1) {
-						System.out.println("Client could not recover");
-						this.model.changeCurrentState(ModelState.PROMPTRELOAD);
-						return;
-					}
-				}
-				if (Thread.interrupted()) {
-					this.model.changeCurrentState(ModelState.PROMPTRELOAD);
-					return;
-				}
-				System.out.println("Client is awake, trying to restore connection");
-				endTime = System.currentTimeMillis();
-			} else {
-				System.out.println("Client could not recover in a timely fashion. ");
-				this.model.changeCurrentState(ModelState.PROMPTRELOAD);
-				return;
-			}
-
-		}
-
-		if (!this.model.getCurrentState().equals(ModelState.EXIT)) {
-			try {
-				toServer = new ObjectOutputStream(s.getOutputStream());
-			} catch (IOException e) {
-				System.out.println("Could not create output stream to server");
-			}
-
-			try {
-				fromServer = new ObjectInputStream(s.getInputStream());
-			} catch (IOException e) {
-				System.out.println("Could not create input stream to server");
-			}
-		} else {
-			System.out.println("Goodbye!");
-			return;
-		}
-
-	}
-
-	/**
-	 * @return the fromServer
-	 */
-	public ObjectInputStream getFromServer() {
-		return fromServer;
-	}
-
-	/**
-	 * @return the model
-	 */
-	public Model getModel() {
-		return model;
 	}
 
 	public static void main(String[] args) {
