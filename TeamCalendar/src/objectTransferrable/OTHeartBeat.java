eb9842d4fe5be42dfe7b922757d03425e8e10608
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-15 23:45:15 +0000

    HEARTBEAT BITCHES

--- TeamCalendar/src/objectTransferrable/OTHeartBeat.java
+++ TeamCalendar/src/objectTransferrable/OTHeartBeat.java
@@ -3,20 +3,54 @@
  */
 package objectTransferrable;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Heartbeat function for checking connection is maintained, could be adapted into update check function if
  * functionality is expanded
  * @author tmd668
  *
  */
-public class OTHeartBeat extends ObjectTransferrable {
+public class OTHeartBeat extends ObjectTransferrable implements Future{
 
 	/**
 	 * @param opCode
 	 */
 	public OTHeartBeat() {
 		super("0014");
-		
+	}
+
+	@Override
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get() throws InterruptedException, ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
