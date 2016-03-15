a304eb115157db5d99b805f4b3bba33b9449efde
nlm233 <nlm233@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-15 21:37:32 +0000

    Made ListPanel look better

--- TeamCalendar/src/gui/ListPanel.java
+++ TeamCalendar/src/gui/ListPanel.java
@@ -6,9 +6,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -18,18 +15,14 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
-import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 
 import client.Client;
-import listener.interfaces.MouseClickedListener;
 import model.Model;
 import model.ModelState;
 import objectTransferrable.Event;
@@ -41,27 +34,11 @@ public class ListPanel extends JPanel{
 	 */
 	private static final long serialVersionUID = -3193985081542643253L;
 	Client controller;
-	static Model model;
+	Model model;
 	JPanel left = new JPanel();
-	Date sqldate;
 	
 	JPanel list = new JPanel();
 	JScrollPane listscroll;
-	
-	JPanel event = new JPanel();
-	JLabel name = new JLabel("Event name");
-	JLabel eventName;
-	JLabel empty = new JLabel("");
-	JLabel descriptionLabel = new JLabel("Description");
-	JLabel decription;
-	JLabel dateLabel = new JLabel("Date");
-	JLabel dateA;
-	JLabel timeLabel = new JLabel("Time");
-	JLabel timeA;
-	JLabel locationLabel = new JLabel("Location");
-	JLabel locationA;
-	JButton edit = new JButton("Edit Event");
-	JButton submit = new JButton("Add Event");
 	
 	Calendar c = new GregorianCalendar();
 	
@@ -73,7 +50,7 @@ public class ListPanel extends JPanel{
 	JButton previous = new JButton("Previous Day");
 	JButton next = new JButton("Next Day");
 	
-	static TitledBorder title;
+	static TitledBorder border;
 	
 	ArrayList<JLabel> times = new ArrayList<JLabel>();
 	static ArrayList<JPanel> events = new ArrayList<JPanel>();
@@ -88,87 +65,58 @@ public class ListPanel extends JPanel{
 		this.model = model;
 		//clickedEvent = new Event(null);
 		
+		//sets dimension and layout of the panel
 		Dimension dimension = new Dimension(Toolkit.getDefaultToolkit().getScreenSize());
 		setPreferredSize(new Dimension((int)dimension.getWidth(), (int)(dimension.getHeight()-200)));
 		setMinimumSize(new Dimension((int)dimension.getWidth(), (int)dimension.getHeight()-200));
 		setMaximumSize(new Dimension((int)dimension.getWidth(), (int)dimension.getHeight()-200));
-		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
-		date=getDate(c.get(Calendar.DAY_OF_WEEK), c.get(Calendar.DATE), c.get(Calendar.MONTH), c.get(Calendar.YEAR));
-		
-//		date.setPreferredSize(new Dimension(100,50));
+		//creates JLabel with the date on
+		date = getDate(c.get(Calendar.DAY_OF_WEEK), c.get(Calendar.DATE), c.get(Calendar.MONTH), c.get(Calendar.YEAR));
 		date.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 25));
 		date.setForeground(Color.DARK_GRAY);
 		
+		//creates addEvent button
 		addEvent.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 24));
 		addEvent.setMinimumSize(new Dimension(35,35));
 		addEvent.setMaximumSize(new Dimension(35,35));
 		
-		top.setMaximumSize(new Dimension(600,70));
+		//creates panel containing date label and addEvent button
+		top.setMaximumSize(new Dimension(900,70));
+		top.setMinimumSize(new Dimension(900,70));
 		top.setLayout(new BoxLayout(top, BoxLayout.LINE_AXIS));
 		top.add(date);
 		top.add(Box.createRigidArea(new Dimension(20,0)));
 		top.add(addEvent);
 		
+		//updates model with the meetings for today and adds them to panel list
 		model.updateMeetings(new Date(c.getTimeInMillis()));
-		
 		addMeetings(model.getMeetings());
 		
+		//adds list to listscroll and sets the size
 		listscroll = new JScrollPane(list);
-		listscroll.setPreferredSize(new Dimension(660,500));
-		listscroll.setMaximumSize(new Dimension(660,500));
-		listscroll.setMinimumSize(new Dimension(660,500));
+		listscroll.setPreferredSize(new Dimension(900,450));
+		listscroll.setMaximumSize(new Dimension(900,450));
+		listscroll.setMinimumSize(new Dimension(900,450));
 		listscroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		listscroll.setAlignmentY(Component.TOP_ALIGNMENT);
 		
-		
-		event.setLayout(new GridLayout(7,2));
-		event.setPreferredSize(new Dimension(400,200));
-		event.setMinimumSize(new Dimension(400,200));
-		event.setMaximumSize(new Dimension(400,200));
-		
-		name.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
-		descriptionLabel.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 13));
-		dateLabel.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 13));
-		timeLabel.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 13));
-		locationLabel.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 13));
-		
-		name.setForeground(Color.DARK_GRAY);
-		descriptionLabel.setForeground(Color.GRAY);
-		dateLabel.setForeground(Color.GRAY);
-		timeLabel.setForeground(Color.GRAY);
-		locationLabel.setForeground(Color.GRAY);
-		
-		changeDay.setPreferredSize(new Dimension(610,50));
+		//creates panel for next and previous buttons
+		changeDay.setPreferredSize(new Dimension(900,50));
 		changeDay.setLayout(new BoxLayout(changeDay, BoxLayout.LINE_AXIS));
 		changeDay.add(previous);
-		top.add(Box.createRigidArea(new Dimension(370,0)));
 		changeDay.add(next);
 		
-		left.setLayout(new BoxLayout(left, BoxLayout.PAGE_AXIS));
-		left.setPreferredSize(new Dimension(750, 700));
+		//creates
 		top.setAlignmentX(Component.CENTER_ALIGNMENT);
 		listscroll.setAlignmentX(Component.CENTER_ALIGNMENT);
 		changeDay.setAlignmentX(Component.CENTER_ALIGNMENT);
-		left.add(top);
-		left.add(listscroll);
-		left.add(changeDay);
-		
-		add(Box.createRigidArea(new Dimension(50,0)));
-		add(left);
-		add(Box.createRigidArea(new Dimension(50,0)));
-		add(event);
+		add(top);
+		add(listscroll);
+		add(changeDay);
 		
 		//----------------------Listeners----------------------//
-		
-		for(int i=0; i<events.size(); i++){
-			events.get(i).addMouseListener((MouseClickedListener) (e) ->{
-//				setEventClickedOn();
-				setEvent(event, 2);
-				model.changeCurrentState(ModelState.EVENTSUPDATE);
-				
-			});
-		}
 		
 		previous.addActionListener((e) -> {
 			c.add(Calendar.DATE, -1);
@@ -189,52 +137,12 @@ public class ListPanel extends JPanel{
 			
 		});
 		
-		addEvent.addActionListener((e) -> setEvent(event, 1));
+//		addEvent.addActionListener((e) -> );
 		
 //		submit.addActionListener((e) -> ;
 	}
 	
-	
-	//---------------------Change Event Panel----------------------------//
-	
-	public void setEvent(JPanel event, int a) {
-		if(a==1){
-//			set panel as add new event
-			event.add(name);
-			event.add(new JTextField());
-			event.add(descriptionLabel);
-			event.add(new JTextField());
-			event.add(dateLabel);
-			event.add(new JTextField());
-			event.add(timeLabel);
-			event.add(new JTextField());
-			event.add(locationLabel);
-			event.add(new JTextField());
-			event.add(submit);
-			
-		} else {
-//			set panel as event clicked on
-			eventName = new JLabel();
-			decription = new JLabel();
-			dateA = new JLabel();
-			timeA = new JLabel();
-			locationA = new JLabel();
-			
-			event.add(eventName);
-			event.add(empty);
-			event.add(descriptionLabel);
-			event.add(decription);
-			event.add(dateLabel);
-			event.add(dateA);
-			event.add(timeLabel);
-			event.add(timeA);
-			event.add(locationLabel);
-			event.add(locationA);
-			event.add(edit);
-		}
-		
-		this.event = event;
-	}
+
 	
 	//----------------------Change Date at the top of page------------------------------//
 	
@@ -265,32 +173,56 @@ public class ListPanel extends JPanel{
 			for(int i=0; i<arraylist.size(); i++){
 			
 				//creates new JLabel of event name for each event
-				JLabel m = new JLabel(arraylist.get(i).getEventTitle(), SwingConstants.LEFT); 
-				m.setVerticalAlignment(SwingConstants.CENTER);
-				m.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
-				m.setForeground(Color.DARK_GRAY);
+				JLabel title = new JLabel(arraylist.get(i).getEventTitle(), SwingConstants.LEFT);
+				String a = "Notes :  " + arraylist.get(i).getEventDescription();
+				JLabel description = new JLabel(a);
+				String b = "Location :  " + arraylist.get(i).getLocation();
+				JLabel location = new JLabel(b);
+				JButton edit = new JButton("Edit event");
+				
+				title.setVerticalAlignment(SwingConstants.CENTER);
+				description.setVerticalAlignment(SwingConstants.CENTER);
+				location.setVerticalAlignment(SwingConstants.CENTER);
+				edit.setVerticalAlignment(SwingConstants.CENTER);
+				
+				title.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
+				description.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
+				location.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
+				edit.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 13));
+				
+				title.setForeground(Color.RED);
+				description.setForeground(Color.DARK_GRAY);
+				location.setForeground(Color.DARK_GRAY);
 			
 				// creates title border with time of each event
-				String s = arraylist.get(i).getStartTime().toString().substring(0, 5);
-				title = BorderFactory.createTitledBorder(s);
-				Border line = BorderFactory.createLineBorder(Color.red);
-				title.setTitleFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
+				String s = arraylist.get(i).getStartTime().toString().substring(0, 5) + " - " + arraylist.get(i).getEndTime().toString().substring(0, 5);
+//				Border line = BorderFactory.createLineBorder(Color.red);
+				border = BorderFactory.createTitledBorder(s);
+				border.setTitleFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
 				
 				// creates new JPanel with title border
 				JPanel p = new JPanel();
-				p.setBorder(title);
+				p.setMaximumSize(new Dimension(890, 200));
+				p.setMinimumSize(new Dimension(890, 200));
+				p.setBorder(border);
+				p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
 			
 				//adds Jlabel to JPanel and adds to events arraylist 
-				p.add(m);
+				p.add(title);
+				p.add(Box.createRigidArea(new Dimension(0,10)));
+				p.add(location);
+				p.add(Box.createRigidArea(new Dimension(0,5)));
+				p.add(description);
+				p.add(Box.createRigidArea(new Dimension(0,10)));
+				p.add(edit);
 				events.add(p);	
 			}
 			
-		
-			list.setPreferredSize(new Dimension(650,800));
-			list.setLayout(new GridLayout(events.size(),1));
+			list.setLayout(new BoxLayout(list, BoxLayout.PAGE_AXIS));
 			
 			for(int i = 0; i<events.size(); i++){
 				list.add(events.get(i));
+				list.add(Box.createRigidArea(new Dimension(0,20)));
 			}
 		}
 		
