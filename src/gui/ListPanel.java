64c2e3161d5a505fcf8788f81ccc7ce5866cde10
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-15 21:58:23 +0000

    List Panel can now update meetings. Adding a new event's button visible as a plus. Adding a new event button will now bring up a new form for filling in event information.

--- src/gui/ListPanel.java
+++ src/gui/ListPanel.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
@@ -37,8 +39,23 @@ public class ListPanel extends JPanel{
 	Model model;
 	JPanel left = new JPanel();
 	
-	JPanel list = new JPanel();
+	JPanel eventsListPanel = new JPanel();
 	JScrollPane listscroll;
+	
+	JPanel event = new JPanel();
+	JLabel name = new JLabel("Event title");
+	JLabel eventName;
+	JLabel empty = new JLabel("");
+	JLabel descriptionLabel = new JLabel("Description");
+	JLabel decription;
+	JLabel dateLabel = new JLabel("Date");
+	JLabel dateA;
+	JLabel timeLabel = new JLabel("Time");
+	JLabel timeA;
+	JLabel locationLabel = new JLabel("Location");
+	JLabel locationA;
+	JButton edit = new JButton("Edit Event");
+	JButton submit = new JButton("Add Event");
 	
 	Calendar c = new GregorianCalendar();
 	
@@ -72,13 +89,16 @@ public class ListPanel extends JPanel{
 		setMaximumSize(new Dimension((int)dimension.getWidth(), (int)dimension.getHeight()-200));
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
-		//creates JLabel with the date on
-		date = getDate(c.get(Calendar.DAY_OF_WEEK), c.get(Calendar.DATE), c.get(Calendar.MONTH), c.get(Calendar.YEAR));
+		date= new JLabel(getDate(c.get(Calendar.DAY_OF_WEEK), c.get(Calendar.DATE), c.get(Calendar.MONTH), c.get(Calendar.YEAR)));
+		
+//		date.setPreferredSize(new Dimension(100,50));
 		date.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 25));
 		date.setForeground(Color.DARK_GRAY);
 		
 		//creates addEvent button
-		addEvent.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 24));
+		addEvent.setMargin(new Insets(0, 0, 0, 0));
+		addEvent.setBackground(Color.PINK);
+		addEvent.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 25));
 		addEvent.setMinimumSize(new Dimension(35,35));
 		addEvent.setMaximumSize(new Dimension(35,35));
 		
@@ -95,7 +115,7 @@ public class ListPanel extends JPanel{
 		addMeetings(model.getMeetings());
 		
 		//adds list to listscroll and sets the size
-		listscroll = new JScrollPane(list);
+		listscroll = new JScrollPane(eventsListPanel);
 		listscroll.setPreferredSize(new Dimension(900,450));
 		listscroll.setMaximumSize(new Dimension(900,450));
 		listscroll.setMinimumSize(new Dimension(900,450));
@@ -116,57 +136,135 @@ public class ListPanel extends JPanel{
 		add(listscroll);
 		add(changeDay);
 		
+		event.add(name);
+		event.add(descriptionLabel);
+		event.add(dateLabel);
+		event.add(timeLabel);
+		event.add(locationLabel);
+		
+		
 		//----------------------Listeners----------------------//
 		
 		previous.addActionListener((e) -> {
 			c.add(Calendar.DATE, -1);
-			setDate(date, c);
+			setDate();
 			model.updateMeetings(new Date(c.getTimeInMillis()));
-			
 			addMeetings(model.getMeetings());
 			model.changeCurrentState(ModelState.EVENTSUPDATE);
 		});
 		
 		next.addActionListener((e) -> {
 			c.add(Calendar.DATE, 1);
-			setDate(date, c);
+			setDate();
 			model.updateMeetings(new Date(c.getTimeInMillis()));
-			
 			addMeetings(model.getMeetings());
 			model.changeCurrentState(ModelState.EVENTSUPDATE);
 			
 		});
 		
-//		addEvent.addActionListener((e) -> );
+		addEvent.addActionListener((e) -> {
+			JPanel eventPopup = new JPanel();
+			event.setLayout(new GridLayout(7,2));
+			event.setPreferredSize(new Dimension(400,200));
+			event.setMinimumSize(new Dimension(400,200));
+			event.setMaximumSize(new Dimension(400,200));
+			setEvent(eventPopup, 1);
+		});
 		
 //		submit.addActionListener((e) -> ;
 	}
 	
 
 	
-	//----------------------Change Date at the top of page------------------------------//
+	//---------------------Change Event Panel----------------------------//
 	
-	public void setDate(JLabel date, Calendar c){
-		date = getDate(c.get(Calendar.DAY_OF_WEEK), c.get(Calendar.DATE), c.get(Calendar.MONTH), c.get(Calendar.YEAR));
-		this.date = date;
+	public void setEvent(JPanel event, int a) {
+		if(a==1){
+			
+			event.setLayout(new GridLayout(7,2));
+			event.setPreferredSize(new Dimension(400,200));
+			event.setMinimumSize(new Dimension(400,200));
+			event.setMaximumSize(new Dimension(400,200));
+			
+//			set panel as add new event
+			event.add(name);
+			JTextField newEventTitle = new JTextField();
+			event.add(newEventTitle);
+			
+			event.add(descriptionLabel);
+			JTextField newEventDescription = new JTextField();
+			event.add(newEventDescription);
+		
+			event.add(dateLabel);
+			JTextField newEventDate= new JTextField();
+			event.add(newEventDate);
+			
+			event.add(timeLabel);
+			JTextField newEventTime= new JTextField();
+			event.add(newEventTime);
+			
+			event.add(locationLabel);
+			JTextField newEventLocation  = new JTextField();
+			event.add(newEventLocation);
+
+			event.add(submit);
+			int result = JOptionPane.showConfirmDialog(this, event, "Add event", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+			 if (result == JOptionPane.OK_OPTION) {
+				 
+		           model.addEvents(newEventTitle.getText(), newEventDescription.getText(), 
+		        		   newEventDate.getText(), newEventTime.getText(), newEventLocation.getText());
+		        } 
+			
+		} else {
+//			set panel as event clicked on
+			eventName = new JLabel();
+			decription = new JLabel();
+			dateA = new JLabel();
+			timeA = new JLabel();
+			locationA = new JLabel();
+			
+			event.add(eventName);
+			event.add(empty);
+			event.add(descriptionLabel);
+			event.add(decription);
+			event.add(dateLabel);
+			event.add(dateA);
+			event.add(timeLabel);
+			event.add(timeA);
+			event.add(locationLabel);
+			event.add(locationA);
+			event.add(edit);
+		}
+		
+		this.event = event;
+		this.revalidate();
+		this.repaint();
 	}
 	
+	//----------------------Change Date at the top of page------------------------------//
 	
+	public void setDate(){
+		this.date.setText(getDate(c.get(Calendar.DAY_OF_WEEK), c.get(Calendar.DATE), c.get(Calendar.MONTH), c.get(Calendar.YEAR)));
+		this.revalidate();
+		this.repaint();
+		
+	}
+	
+		
 	//---------------------Add meeting panels to listscroll-------------------------//
 
 	public void addMeetings(ArrayList<Event> arraylist){
-			
-		JPanel list = new JPanel();
+		this.eventsListPanel.removeAll();
 		events.clear();
 		if (arraylist.isEmpty()){
 			
-			list.setPreferredSize(new Dimension(650,800));
-			list.setLayout(new GridLayout(1,1));
+			eventsListPanel.setPreferredSize(new Dimension(650,800));
+			eventsListPanel.setLayout(new GridLayout(1,1));
 			
 			JLabel l = new JLabel("You have no events right now!");
 			l.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
 			l.setForeground(Color.RED);
-			list.add(l);
+			eventsListPanel.add(l);
 			
 		} else {
 			
@@ -218,15 +316,17 @@ public class ListPanel extends JPanel{
 				events.add(p);	
 			}
 			
-			list.setLayout(new BoxLayout(list, BoxLayout.PAGE_AXIS));
+		
+			eventsListPanel.setPreferredSize(new Dimension(650,800));
+			eventsListPanel.setLayout(new GridLayout(events.size(),1));
 			
 			for(int i = 0; i<events.size(); i++){
-				list.add(events.get(i));
+				eventsListPanel.add(events.get(i));
 				list.add(Box.createRigidArea(new Dimension(0,20)));
 			}
 		}
 		
-		this.list=list;
+		
 	}
 	
 	//---------------------Says which event is clicked on--------------//
@@ -238,7 +338,7 @@ public class ListPanel extends JPanel{
 	
 	//-------------------Creates JLabel for date-----------------------//
 		
-	public static JLabel getDate(int day, int date, int month, int year){
+	public static String getDate(int day, int date, int month, int year){
 		
 		StringBuffer s = new StringBuffer();
 		
@@ -281,7 +381,11 @@ public class ListPanel extends JPanel{
 		
 		s.append(" " + year);
 		
-		return new JLabel(s.toString());
+		return s.toString();
+		
+	}
+	
+	public void getEventsListPanel(){
 		
 	}
 
