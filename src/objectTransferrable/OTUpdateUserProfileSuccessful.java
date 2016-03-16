04a5dcb22f72c3384e47fe7f261aa3b662a9ef8d
mxw589 <mxw589@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-16 16:17:44 +0000

    Updated the update profile user information so that it properly updates the corresponding variables in the model

--- src/objectTransferrable/OTUpdateUserProfileSuccessful.java
+++ src/objectTransferrable/OTUpdateUserProfileSuccessful.java
@@ -2,8 +2,27 @@ package objectTransferrable;
 
 public class OTUpdateUserProfileSuccessful extends ObjectTransferrable{
 
-	public OTUpdateUserProfileSuccessful() {
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	public OTUpdateUserProfileSuccessful(String firstName, String lastName, String email) {
 		super("0022");
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
 	}
 
 }
