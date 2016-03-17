2e0711d5f8733b66b5b600834d7d2857807c0dbf
mxw589 <mxw589@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-17 15:46:39 +0000

    QueryManager is now an object shared by all threads, and the updateEvent method is now synchronized

--- src/server/ETRunTask.java
+++ src/server/ETRunTask.java
@@ -34,28 +34,27 @@ public class ETRunTask implements ExecutableTask{
 	@Override
 	public void run() {
 		//create a new QueryManager object to handle the execution of the received object
-		QueryManager runQuery = new QueryManager(getQuery(), getMasterServer(), this.clientInfo);
+		QueryManager runQuery = getMasterServer().getQueryManager();
 
 		//execute the query, updating the object
 		try {
-			runQuery.runOperation();
+			ObjectTransferrable returnObject = runQuery.runOperation(getQuery(), getClientInfo());
+			//get the updated object and pass it back to the client
+			try {
+				if(!returnObject.getOpCode().equals("0014")){
+					getMasterServer().getServerModel().addToText("Sending back Object with opCode " + returnObject.getOpCode() + "\n");
+				}
+				getClientInfo().getClientOutput().writeObject(returnObject);
+				if(!returnObject.getOpCode().equals("0014")){
+					getMasterServer().getServerModel().addToText("SENT" + "\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
-		//get the updated object and pass it back to the client
-		try {
-			if(!runQuery.getOperation().getOpCode().equals("0014")){
-				getMasterServer().getServerModel().addToText("Sending back Object with opCode " + runQuery.getOperation().getOpCode() + "\n");
-			}
-			getClientInfo().getClientOutput().writeObject(runQuery.getOperation());
-			if(!runQuery.getOperation().getOpCode().equals("0014")){
-				getMasterServer().getServerModel().addToText("SENT" + "\n");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}	
-
+		
 		//create a new instance of ETSearchForObject so to pick up more queries from this client and pass it to the threadpool
 		ETSearchForObject newSearch = new ETSearchForObject(getMasterServer(), getClientInfo());
 		getMasterServer().getThreadPool().execute(newSearch);
