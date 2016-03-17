a1c49c06a8c4d86d960ce712374040ee5405ae4b
tmd668 <tmd668@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-17 13:46:39 +0000

    Started the jUnitTests

---
+++ src/jUnitTests/clientModelTests.java
@@ -0,0 +1,14 @@
+package jUnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class clientModelTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
