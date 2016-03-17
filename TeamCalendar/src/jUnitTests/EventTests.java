cef891f78f4372561202369c2160e2f2e2e18c88
tmd668 <tmd668@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-17 13:46:39 +0000

    Started the jUnitTests

---
+++ TeamCalendar/src/jUnitTests/EventTests.java
@@ -0,0 +1,43 @@
+package jUnitTests;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.events.EventException;
+
+import objectTransferrable.Event;
+public class EventTests {
+	private Event testEvent;
+	private Time startTime;
+	private Time endTime;
+	private String description, title, location;
+	private Date date;
+	
+	@Before
+	public void setup(){
+		startTime = Time.valueOf("18:50:00");
+		endTime = Time.valueOf("20:30:00");
+		description = "test description";
+		title = "test title";
+		location = "test location";
+		date = Date.valueOf("2016-06-15");
+		testEvent = new Event(startTime, endTime, description, title, location, date);
+	}
+	
+	@Test
+	public void test() {
+		assertTrue(testEvent.getStartTime().equals(startTime));
+		assertTrue(testEvent.getEndTime().equals(endTime));
+		assertTrue(testEvent.getEventDescription().equals(description));
+		assertTrue(testEvent.getEventTitle().equals(title));
+		assertTrue(testEvent.getLocation().equals(location));
+		assertTrue(testEvent.getDate().equals(date));
+		
+		
+	}
+
+}
