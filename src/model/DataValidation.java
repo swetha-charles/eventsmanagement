d8772849156d99b7c42bf9585c5c982742e54bb3
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-17 19:58:04 +0000

    Data validation methods for ListPanel

--- src/model/DataValidation.java
+++ src/model/DataValidation.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -45,8 +46,89 @@ public class DataValidation {
 		return true;
 	}
 
+	public static boolean isThisDateValid(String day, String month, String year) {
+		if (day.length() == 0 || month.length() == 0 || year.length() != 4) {
+			//user did not input day, month or year correctly
+			return false;
+		} else {
+			try {
+				Integer.parseInt(day);
+				Integer.parseInt(month);
+				Integer.parseInt(year);
+			} catch (NumberFormatException e2) {
+				//user input letters
+				return false;
+			}
+		}
+		if (day.length() == 1) {
+			//in case of unexpected formatting
+			day = "0" + day;
+		}
+		if (month.length() == 1) {
+			//in case of unexpected formatting
+			month = "0" + month;
+		}
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		String fullDate = day + "/" + month + "/" + year;
+		sdf.setLenient(false);
+		try {
+			// if fullDate is not valid, this code will throw ParseException
+			Date date = sdf.parse(fullDate);
+			System.out.println(date);
+		} catch (ParseException e) {
+			return false;
+		}
+		return true;
+	}
+	/**
+	 * returns null if date is not valid
+	 * @param day
+	 * @param month
+	 * @param year
+	 * @return
+	 */
+	public static Date sanitizeDate(String day, String month, String year){
+		Date date = null;
+		if(isThisDateValid(day, month, year)){
+			if (day.length() == 1) {
+				//in case of unexpected formatting
+				day = "0" + day;
+			}
+			if (month.length() == 1) {
+				//in case of unexpected formatting
+				month = "0" + month;
+			}
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			String fullDate = day + "/" + month + "/" + year;
+			try {
+				date = sdf.parse(fullDate);
+			} catch (ParseException e) {
+				return date;
+			}
+		}
+		return date;
+	}
+
+	public static boolean isThisTimeValid(String hours, String minutes) {
+		try {
+			// hours and minutes are both numbers
+			int hoursInt = Integer.parseInt(hours);
+			int minutesInt = Integer.parseInt(minutes);
+			// check hours:
+			if (hoursInt >= 0 && hoursInt <= 24) {
+				if (minutesInt >= 0 && minutesInt <= 60) {
+					return true;
+				}
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("Data validation, isThisTimeValid(): could not parse input received");
+		}
+		// in all other cases, return false
+		return false;
+	}
+
 	public static boolean checkLessThanTwenty(String username) {
-		return (username.length()<= 20);
+		return (username.length() <= 20);
 	}
 
 	public static boolean checkLessThanThirty(String username) {
