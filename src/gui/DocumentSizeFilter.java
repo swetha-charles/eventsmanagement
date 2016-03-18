49e3cf62fa8696e69946907541dfad9e2565ab4c
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-18 15:36:31 +0000

    Data validation for username, password, firstname, lastname, useremail. DocumentSizeFilter checks for the max character count given. It blocks the user from inputting more characters and if it's feeling like it, it *may* beep.

--- src/gui/DocumentSizeFilter.java
+++ src/gui/DocumentSizeFilter.java
@@ -29,8 +29,7 @@ package gui;
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
-
+ */
 
 /* A 1.4 class used by TextComponentDemo.java. */
 
@@ -39,46 +38,40 @@ import javax.swing.text.*;
 import java.awt.Toolkit;
 
 public class DocumentSizeFilter extends DocumentFilter {
-    int maxCharacters;
-    boolean DEBUG = false;
+	int maxCharacters;
+	boolean DEBUG = false;
 
-    public DocumentSizeFilter(int maxChars) {
-        maxCharacters = maxChars;
-    }
+	public DocumentSizeFilter(int maxChars) {
+		maxCharacters = maxChars;
+	}
 
-    public void insertString(FilterBypass fb, int offs,
-                             String str, AttributeSet a)
-        throws BadLocationException {
-        if (DEBUG) {
-            System.out.println("in DocumentSizeFilter's insertString method");
-        }
+	public void insertString(FilterBypass fb, int offs, String str, AttributeSet a) throws BadLocationException {
+		if (DEBUG) {
+			System.out.println("in DocumentSizeFilter's insertString method");
+		}
 
-        //This rejects the entire insertion if it would make
-        //the contents too long. Another option would be
-        //to truncate the inserted string so the contents
-        //would be exactly maxCharacters in length.
-        if ((fb.getDocument().getLength() + str.length()) <= maxCharacters)
-            super.insertString(fb, offs, str, a);
-        else
-            Toolkit.getDefaultToolkit().beep();
-    }
-    
-    public void replace(FilterBypass fb, int offs,
-                        int length, 
-                        String str, AttributeSet a)
-        throws BadLocationException {
-        if (DEBUG) {
-            System.out.println("in DocumentSizeFilter's replace method");
-        }
-        //This rejects the entire replacement if it would make
-        //the contents too long. Another option would be
-        //to truncate the replacement string so the contents
-        //would be exactly maxCharacters in length.
-        if ((fb.getDocument().getLength() + str.length()
-             - length) <= maxCharacters)
-            super.replace(fb, offs, length, str, a);
-        else
-            Toolkit.getDefaultToolkit().beep();
-    }
+		// This rejects the entire insertion if it would make
+		// the contents too long. Another option would be
+		// to truncate the inserted string so the contents
+		// would be exactly maxCharacters in length.
+		if ((fb.getDocument().getLength() + str.length()) <= maxCharacters)
+			super.insertString(fb, offs, str, a);
+		else
+		Toolkit.getDefaultToolkit().beep();
+	}
+
+	public void replace(FilterBypass fb, int offs, int length, String str, AttributeSet a) throws BadLocationException {
+		if (DEBUG) {
+			System.out.println("in DocumentSizeFilter's replace method");
+		}
+		// This rejects the entire replacement if it would make
+		// the contents too long. Another option would be
+		// to truncate the replacement string so the contents
+		// would be exactly maxCharacters in length.
+		if ((fb.getDocument().getLength() + str.length() - length) <= maxCharacters)
+			super.replace(fb, offs, length, str, a);
+		else
+		Toolkit.getDefaultToolkit().beep();
+	}
 
 }
