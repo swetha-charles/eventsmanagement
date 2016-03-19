fbaac04fe4a0b0c3f566d764d47f2870bc330003
tmd668 <tmd668@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-19 18:56:04 +0000

    Client model JUnit test updated

--- src/jUnitTests/clientModelTests.java
+++ src/jUnitTests/clientModelTests.java
@@ -21,8 +21,35 @@ public class clientModelTests {
 	public void testStates() {
 		model.setUpdatePasswordSuccess(true);
 		assertTrue(model.getCurrentState().equals(ModelState.PROFILE));
-		//is the panel supposed to have changed too?
+		model.changeCurrentState(ModelState.REGISTRATION);
+		assertTrue(model.getCurrentState().equals(ModelState.REGISTRATION));
+		model.changeCurrentState(ModelState.LOGIN);
+		assertTrue(model.getCurrentState().equals(ModelState.LOGIN));
+		model.changeCurrentState(ModelState.LOGINUNSUCCESSFULWRONGUSERNAME);
+		assertTrue(model.getCurrentState().equals(ModelState.LOGINUNSUCCESSFULWRONGUSERNAME));
+		model.changeCurrentState(ModelState.LOGINUNSUCCESSFULWRONGPASSWORD);
+		assertTrue(model.getCurrentState().equals(ModelState.LOGINUNSUCCESSFULWRONGPASSWORD));
+		model.changeCurrentState(ModelState.REGISTRATIONUPDATE);
+		assertTrue(model.getCurrentState().equals(ModelState.REGISTRATIONUPDATE));
+		model.changeCurrentState(ModelState.ERRORCONNECTIONDOWN);
+		assertTrue(model.getCurrentState().equals(ModelState.ERRORCONNECTIONDOWN));
+		model.changeCurrentState(ModelState.ERRORCONNECTIONDOWNSTILL);
+		assertTrue(model.getCurrentState().equals(ModelState.ERRORCONNECTIONDOWNSTILL));
+		model.changeCurrentState(ModelState.PROMPTRELOAD);
+		assertTrue(model.getCurrentState().equals(ModelState.PROMPTRELOAD));
 		
+		model.changeCurrentState(ModelState.EVENTSUPDATE);
+		assertTrue(model.getCurrentState().equals(ModelState.EVENTSUPDATE));
+		model.changeCurrentState(ModelState.PROFILE);
+		assertTrue(model.getCurrentState().equals(ModelState.PROFILE));
+		model.changeCurrentState(ModelState.EDIT);
+		assertTrue(model.getCurrentState().equals(ModelState.EDIT));
+		model.changeCurrentState(ModelState.PASSWORD);
+		assertTrue(model.getCurrentState().equals(ModelState.PASSWORD));
 	}
-
+	@Test
+	public void testStatesThatCauseThings() {
+		model.changeCurrentState(ModelState.EVENTS);
+		assertTrue(model.getCurrentState().equals(ModelState.EVENTS));
+	}
 }
