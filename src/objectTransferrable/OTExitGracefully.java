e3caac7a5d972e46e5feee4a0bb9f5db108c9bba
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-19 18:49:12 +0000

    Added a way to time out on the exitConfirmation method in client. This ensures that after sending an exit message to the server, the client isn't indefinitely waiting for a reply.

--- src/objectTransferrable/OTExitGracefully.java
+++ src/objectTransferrable/OTExitGracefully.java
@@ -1,10 +1,44 @@
 package objectTransferrable;
 
-public class OTExitGracefully extends ObjectTransferrable {
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class OTExitGracefully extends ObjectTransferrable  implements Future<Boolean>{
 	private static final long serialVersionUID = 1L;
 	
 	public OTExitGracefully(){
 		super("0005");
 		
 	}
+
+	@Override
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Boolean get() throws InterruptedException, ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+		return true;
+		}
 }
