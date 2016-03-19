2c5b6de21d5990e1f660e5752f9f1dea9ad5270e
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-19 20:13:43 +0000

    Update Object Transferrable to implement Future. This ensure that the client is never waiting for more than a set period of time to hear back from the server. Right now this is set to 5 seconds, can be decreased. To decrease go to class Client: method readFromServer().

--- src/objectTransferrable/ObjectTransferrable.java
+++ src/objectTransferrable/ObjectTransferrable.java
@@ -1,14 +1,49 @@
 package objectTransferrable;
 
-public abstract class ObjectTransferrable implements java.io.Serializable{
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public abstract class ObjectTransferrable implements java.io.Serializable, Future<Boolean> {
 
 	private static final long serialVersionUID = 85731937678276152L;
 	private final String opCode;
 
-		public ObjectTransferrable(String opCode){
-			this.opCode = opCode;
-		}
-	public String getOpCode(){
+	public ObjectTransferrable(String opCode) {
+		this.opCode = opCode;
+	}
+
+	public String getOpCode() {
 		return opCode;
+	}
+
+	@Override
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Boolean get() throws InterruptedException, ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+		return false;
 	}
 }
