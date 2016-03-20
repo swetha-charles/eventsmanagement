a76da1729e6812daaf8595d4526d0dd60c82aa1c
tmd668 <tmd668@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-20 19:14:39 +0000

    Client connection arguments added

--- TeamCalendar/src/client/Client.java
+++ TeamCalendar/src/client/Client.java
@@ -638,7 +638,22 @@ public class Client {
 		return this.error;
 	}
 	public static void main(String[] args) {
-		Client C = new Client(4444, "localhost");
+
+		int port = 50280;
+		String iNetAddress = "localHost" ; 
+		if (args.length == 1){
+				System.out.println("No port specified, using default");
+				iNetAddress = args[0];
+		}
+		else if (args.length >= 2){
+			iNetAddress = args[0];
+			port = Integer.parseInt(args[1]);
+		}
+		else{
+			System.out.println("No arguements supplied, using default address " + iNetAddress + "and port " + port); 
+		}
+		Client C = new Client(port, iNetAddress);
+
 	}
 
 }
