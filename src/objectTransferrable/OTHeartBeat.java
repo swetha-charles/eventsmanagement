10c884596278464bf4f1df501258e1726c93ca80
sxc653 <sxc653@b0e60272-9ff1-4a17-a597-c6b6621f52a3>
2016-03-24 10:13:46 +0000

    Error server down added as a Model State

--- src/objectTransferrable/OTHeartBeat.java
+++ src/objectTransferrable/OTHeartBeat.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package objectTransferrable;
 
 import java.util.concurrent.ExecutionException;
--- src/objectTransferrable/OTUpdateUserProfile.java
+++ src/objectTransferrable/OTUpdateUserProfile.java
@@ -4,6 +4,15 @@ public class OTUpdateUserProfile extends ObjectTransferrable {
 	private String firstName;
 	private String lastName;
 	private String email;
+	
+	/**
+	 * Request sent from client to server indicating that the user
+	 * would like to change their profile information.
+	 * This information has already been validated. 
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 */
 	public OTUpdateUserProfile(String firstName, String lastName, String email) {
 		super("0021");
 		this.firstName = firstName;
